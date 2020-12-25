27469095b102e24b8f2615c092312453a3356c89
tarushjreddy <tarushjreddy63@gamil.com>
2020-12-25 14:46:42 +0530

    version one

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+
+  <HaederPart msg="Welcome to Your Vue.js App"/>
+  <LoginPage/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HaederPart from './components/HelloWorld.vue'
+import LoginPage from './components/login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HaederPart,
+    LoginPage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0px;
 }
 </style>
---
+++ src/components/login.vue
@@ -0,0 +1,63 @@
+<template>
+<div v-if="isOpen">
+  <div class="login_page"  @click="isOpen=false">
+ 
+  </div>
+  <div class="ord">
+
+      <div class="main_one">
+          <div class="ogo" @click="$emit('openLoginMod')">Login</div>
+             
+      </div>
+  </div>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            isOpen: true
+        };
+    }
+}
+</script>
+<style>
+.login_page{
+    color: black;
+    height: 100vh;
+    width: 100vw;
+    background-color: rgb(155, 155, 155);
+    font-family: 'Poppins', sans-serif;
+    position: fixed;
+    top: 0;
+    opacity: 80%;
+    z-index: 30;
+    transition: all 200ms ease-in-out;
+
+}
+.ord{
+    position: absolute;
+     transition: all 200ms ease-in-out;
+
+    top: 0;
+}
+.main_one{
+    display: flex;
+    width: 100%;
+    display: absolute;
+     transition: all 200ms ease-in-out;
+top: 50;
+}
+.ogo{
+background-color: white;
+display: absolute;
+font-size: 30px;
+top: 50;
+padding: 20px;
+margin-left: 50%;
+margin-right: auto;
+ transition: all 200ms ease-in-out;
+  z-index: 40;
+
+}
+</style>
